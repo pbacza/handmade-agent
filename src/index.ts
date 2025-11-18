ac873e1930fe74aae631805839be951c68d39e0a
Pawel Bacza <bacza2@gmail.com>
2025-11-18 22:05:27 +0000

    Mini gemini

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import { config } from "dotenv";
 config({ quiet: true });
 
-console.log("Hello Agent", process.env.G_KEY);
+import { GoogleGenAI } from "@google/genai";
+
+const context:string[] = []
+
+const ai = new GoogleGenAI({ apiKey: process.env.G_KEY ?? '' });
+
+async function main() {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: "Explain how AI works in a few words",
+    });
+
+    console.log(response.text);
+}
+
+await main();
