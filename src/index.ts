26fe6757e298583d87fe81dec07945754dde3bd0
Pawel Bacza <bacza2@gmail.com>
2025-11-22 16:09:08 +0000

    use openai

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,20 @@
 import { config } from 'dotenv';
 config({ quiet: true });
 
-import { GenerateContentResponse, GoogleGenAI, type Content } from '@google/genai';
+import OpenAI from 'openai';
+import type {
+  ResponseInput,
+  Response,
+  ResponseFunctionToolCall,
+  ResponseInputItem,
+} from 'openai/resources/responses/responses.mjs';
 import * as readline from 'readline/promises';
+import { ping, pingu } from './tools/ping.js';
 
-const context: Content[] = [];
+const client = new OpenAI({ apiKey: process.env.O_KEY });
+const context: ResponseInput = [];
 
-const ai = new GoogleGenAI({ apiKey: process.env.G_KEY ?? '' });
-
-const callLLM = async (line: string): Promise<GenerateContentResponse> => {
-  context.push({ role: 'user', parts: [{ text: line }] });
-
-  const response = await ai.models.generateContent({
-    model: 'gemini-2.5-flash',
-    contents: context,
-  });
-
-  context.push({ role: 'model', parts: [{ text: response.text ?? '' }] });
-
-  return response;
-};
+main();
 
 async function main() {
   const rl = readline.createInterface({
@@ -28,10 +23,92 @@ async function main() {
   });
 
   while (true) {
-    const line = await rl.question('>!< ');
-    const response = await callLLM(line);
-    console.log(response.text);
+    const line = await rl.question('>?<: ');
+    const response = await processInput(line);
+    console.log('^!^: ', response);
   }
 }
 
-await main();
+const processInput = async (line: string) => {
+  context.push({ role: 'user', content: line });
+
+  let response = await callLLM(context);
+  context.push({ role: 'assistant', content: response.output_text });
+
+  const hasCalledTool = await handleTools(response);
+  if (hasCalledTool) {
+    response = await callLLM(context);
+  }
+
+  return response.output_text;
+};
+
+const callLLM = (context: OpenAI.Responses.ResponseInput) => {
+  return client.responses.create({
+    model: 'gpt-5',
+    input: context,
+    // reasoning: { effort: 'medium' },
+    tools: [
+      {
+        name: pingu.name,
+        description: pingu.description,
+        type: 'function',
+        parameters: {
+          type: 'object',
+          properties: {
+            [pingu.params.name]: {
+              type: 'string',
+              description: pingu.params.description,
+            },
+          },
+          required: [pingu.params.name],
+          additionalProperties: false,
+        },
+        strict: true,
+      },
+    ],
+  });
+};
+
+const handleTools = async (response: Response) => {
+  let hasFnCallHappened = false; //! REFACTOR
+
+  for (const output of response.output) {
+    switch (output.type) {
+      case 'function_call': {
+        context.push(output);
+        const result = await callTool(output);
+        if (result) {
+          context.push(result);
+        }
+        hasFnCallHappened = true;
+        continue;
+      }
+
+      case 'reasoning': {
+        context.push(output);
+        continue;
+      }
+    }
+  }
+
+  return hasFnCallHappened;
+};
+
+const callTool = async (
+  output: ResponseFunctionToolCall,
+): Promise<ResponseInputItem.FunctionCallOutput | undefined> => {
+  if (output.name === 'ping') {
+    const host = JSON.parse(output.arguments).host; //! A BIT UGLY
+    const result = await ping(host);
+    console.log('>>> Ping: ', host);
+
+    return {
+      type: 'function_call_output',
+      call_id: output.call_id,
+      output: JSON.stringify(result),
+    };
+  }
+};
+
+// describe connectivity to google
