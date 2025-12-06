b22dd7f0761ce2f895923f9a8f205fa16af7d3ad
Pawel Bacza <bacza2@gmail.com>
2025-12-06 19:38:12 +0000

    Use local ollama

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { config } from 'dotenv';
 config({ quiet: true });
 
-import { Ollama, type Message, type ChatResponse, type ToolCall } from 'ollama';
-
+import OpenAI from 'openai';
+import type {
+  ResponseInput,
+  Response,
+  ResponseFunctionToolCall,
+  ResponseInputItem,
+} from 'openai/resources/responses/responses.mjs';
 import * as readline from 'readline/promises';
 import { styleText } from 'node:util';
 import { readFileContent, readFileTool } from './tools/read-file.js';
@@ -11,17 +16,9 @@ import { writeFileContent, writeFileTool } from './tools/write-file.js';
 
 const astIcon = styleText('blue', '^!^');
 const usrIcon = styleText('magenta', '>?<');
-const llm = new Ollama();
+const client = new OpenAI({ apiKey: process.env.O_KEY });
 
-const context: Message[] = [
-  {
-    role: 'system',
-    content: `Your source code is in ./src folder.
-    You can use read-directory and read-files to explore existing folders and files.
-    And use write-file to generate new code.
-  `,
-  },
-];
+const context: ResponseInput = [{ role: 'system', content: 'Your source code is in ./src folder' }];
 
 main();
 
@@ -36,7 +33,7 @@ async function main() {
       const line = await rl.question(`${usrIcon}: `);
       context.push({ role: 'user', content: line });
       const response = await processInput(context);
-      console.log(`${astIcon}:`, response.message.content);
+      console.log(`${astIcon}:`, response.output_text);
     } catch (error) {
       // Handle Ctrl+C gracefully
       if (error instanceof Error && error.name === 'AbortError') {
@@ -49,145 +46,154 @@ async function main() {
   }
 }
 
-const processInput = async (context: Message[]): Promise<ChatResponse> => {
+const processInput = async (context: ResponseInput): Promise<OpenAI.Responses.Response> => {
   const response = await callLLM(context);
-  context.push({ role: 'assistant', content: response.message.content });
+  context.push({ role: 'assistant', content: response.output_text });
 
-  if (response.message.tool_calls) {
-    const { hasCalledTool, innerContext } = await handleTools(response.message.tool_calls);
-    context.push(...innerContext);
-    if (hasCalledTool) {
-      console.log('>>> hasCalledTool', hasCalledTool);
-      const rs = await processInput(context);
-      return rs;
-    }
+  const { hasCalledTool, innerContext } = await handleTools(response);
+  context.push(...innerContext);
+  if (hasCalledTool) {
+    console.log('>>> hasCalledTool', hasCalledTool);
+    const rs = await processInput(context);
+    return rs;
   }
 
   return response;
 };
 
-const callLLM = (context: Message[]) => {
-  return llm.chat({
-    model: 'qwen3-vl:8b',
-    messages: context,
-    stream: false,
+const callLLM = (context: OpenAI.Responses.ResponseInput) => {
+  return client.responses.create({
+    model: 'gpt-5-mini',
+    input: context,
+    reasoning: { effort: 'medium' },
     tools: [
       {
+        name: readFileTool.name,
+        description: readFileTool.description,
         type: 'function',
-        function: {
-          name: readFileTool.name,
-          description: readFileTool.description,
-          type: 'function',
-          parameters: {
-            type: 'object',
-            properties: {
-              [readFileTool.params.name]: {
-                type: 'string',
-                description: readFileTool.params.description,
-              },
+        parameters: {
+          type: 'object',
+          properties: {
+            [readFileTool.params.name]: {
+              type: 'string',
+              description: readFileTool.params.description,
             },
-            required: [readFileTool.params.name],
           },
+          required: [readFileTool.params.name],
+          additionalProperties: false,
         },
+        strict: true,
       },
       {
+        name: writeFileTool.name,
+        description: writeFileTool.description,
         type: 'function',
-        function: {
-          name: writeFileTool.name,
-          description: writeFileTool.description,
-          type: 'function',
-          parameters: {
-            type: 'object',
-            properties: {
-              [writeFileTool.params.filePath.name]: {
-                type: 'string',
-                description: writeFileTool.params.filePath.description,
-              },
-              [writeFileTool.params.content.name]: {
-                type: 'string',
-                description: writeFileTool.params.content.description,
-              },
+        parameters: {
+          type: 'object',
+          properties: {
+            [writeFileTool.params.filePath.name]: {
+              type: 'string',
+              description: writeFileTool.params.filePath.description,
             },
-            required: [writeFileTool.params.filePath.name, writeFileTool.params.content.name],
+            [writeFileTool.params.content.name]: {
+              type: 'string',
+              description: writeFileTool.params.content.description,
+            },
           },
+          required: [writeFileTool.params.filePath.name, writeFileTool.params.content.name],
+          additionalProperties: false,
         },
+        strict: true,
       },
       {
+        name: readDirectoryTool.name,
+        description: readDirectoryTool.description,
         type: 'function',
-        function: {
-          name: readDirectoryTool.name,
-          description: readDirectoryTool.description,
-          type: 'function',
-          parameters: {
-            type: 'object',
-            properties: {
-              [readDirectoryTool.params.name]: {
-                type: 'string',
-                description: readDirectoryTool.params.description,
-              },
+        parameters: {
+          type: 'object',
+          properties: {
+            [readDirectoryTool.params.name]: {
+              type: 'string',
+              description: readDirectoryTool.params.description,
             },
-            required: [readDirectoryTool.params.name],
           },
+          required: [readDirectoryTool.params.name],
+          additionalProperties: false,
         },
+        strict: true,
       },
     ],
   });
 };
 
 const handleTools = async (
-  toolCalls: ToolCall[],
-): Promise<{ hasCalledTool: boolean; innerContext: Message[] }> => {
+  response: Response,
+): Promise<{ hasCalledTool: boolean; innerContext: ResponseInput }> => {
   let hasCalledTool = false;
 
-  const innerContext: Message[] = [];
-  for (const toolCall of toolCalls) {
-    const result = await callTool(toolCall);
-    if (result) {
-      innerContext.push(result);
-    }
+  const innerContext: ResponseInput = [];
 
-    hasCalledTool = true;
+  for (const output of response.output) {
+    switch (output.type) {
+      case 'reasoning': {
+        innerContext.push(output);
+        continue;
+      }
+
+      case 'function_call': {
+        innerContext.push(output);
+        const result = await callTool(output);
+        if (result) {
+          innerContext.push(result);
+        }
+        hasCalledTool = true;
+        continue;
+      }
+    }
   }
 
   return { hasCalledTool, innerContext };
 };
 
-const callTool = async (output: ToolCall): Promise<Message | undefined> => {
-  switch (output.function.name) {
+const callTool = async (
+  output: ResponseFunctionToolCall,
+): Promise<ResponseInputItem.FunctionCallOutput | undefined> => {
+  switch (output.name) {
     case readFileTool.name: {
-      const fileName = output.function.arguments.filePath;
+      const fileName = JSON.parse(output.arguments).filePath;
       console.log('>>> Read File: ', fileName);
       const result = await readFileContent(fileName);
 
       return {
-        role: 'tool',
-        tool_name: output.function.name,
-        content: result,
+        type: 'function_call_output',
+        call_id: output.call_id,
+        output: result,
       };
     }
 
     case readDirectoryTool.name: {
-      const dirPath = output.function.arguments.dirPath;
+      const dirPath = JSON.parse(output.arguments).dirPath;
       console.log('>>> Read Dir: ', dirPath);
       const result = await readDirectory(dirPath);
       console.log('>>> Read Dir: ', result);
 
       return {
-        role: 'tool',
-        tool_name: output.function.name,
-        content: JSON.stringify(result),
+        type: 'function_call_output',
+        call_id: output.call_id,
+        output: JSON.stringify(result),
       };
     }
 
     case writeFileTool.name: {
-      const { filePath, content } = output.function.arguments;
+      const { filePath, content } = JSON.parse(output.arguments);
       console.log('>>> Write File: ', filePath);
       await writeFileContent(filePath, content);
 
       return {
-        role: 'tool',
-        tool_name: output.function.name,
-        content: `${filePath} was created`,
+        type: 'function_call_output',
+        call_id: output.call_id,
+        status: 'completed',
+        output: `${filePath} created`,
       };
     }
 
